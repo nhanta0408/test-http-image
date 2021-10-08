22258defc7dbd5f1b8beaefbfa8de7bb1d627b88
nhanta0408 <nhanta04081999@gmail.com>
2021-10-08 19:16:27 +0700

    First commit

---
+++ nodeJs-http-image-server-rn/index.js
@@ -0,0 +1,41 @@
+// server.js
+
+const express = require("express");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+var fs = require("fs");
+var upload = multer({ dest: "uploads/" });
+
+const app = express();
+//app.use(bodyParser.json());
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "./images");
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  },
+});
+
+//const upload = multer({ storage });
+
+app.get("/", (req, res) => {
+  res.status(200).send("You can post to /api/upload.");
+});
+
+app.post("/api/upload", upload.single("photo"), (req, res) => {
+  console.log("file", req.photo);
+  //console.log("body", req.body);
+  // console.log("body", req.body.photo.originalname);
+
+  res.status(200).json({
+    message: "success!",
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `server is running at http://localhost:${process.env.PORT || 3000}`
+  );
+});
