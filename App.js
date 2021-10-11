f2bf3ca5588adaa09d01159e82f5b71c6c947ce2
nhanta0408 <nhanta04081999@gmail.com>
2021-10-11 16:59:31 +0700

    Da hoat dong ok

--- App.js
+++ App.js
@@ -1,100 +1,23 @@
-// import React from 'react';
-// import {View, Image, Button, Platform} from 'react-native';
-// import {launchImageLibrary} from 'react-native-image-picker';
-
-// const SERVER_URL = 'https://e3b9-115-76-156-37.ngrok.io';
-// //const SERVER_URL = 'http://192.168.1.8:3000';
-
-// const createFormData = (photo, body = {}) => {
-//   const data = new FormData();
-
-//   data.append('photo', {
-//     name: photo.fileName,
-//     type: photo.type,
-//     uri: photo.uri,
-//   });
-
-//   Object.keys(body).forEach(key => {
-//     data.append(key, body[key]);
-//   });
-//   console.log(data);
-//   return data;
-// };
-
-// const App = () => {
-//   const [photo, setPhoto] = React.useState(null);
-
-//   const handleChoosePhoto = () => {
-//     launchImageLibrary({noData: true}, response => {
-//       // console.log(response);
-//       if (response) {
-//         setPhoto(response);
-//       }
-//     });
-//   };
-
-//   const handleUploadPhoto = () => {
-//     let body = new FormData();
-//     body.append('photo', {
-//       uri: photo.assets[0].uri,
-//       name: photo.assets[0].name,
-//       type: photo.assets[0].type,
-//     });
-//     body.append('userId', '456');
-//     console.log(body);
-//     fetch(`http://192.168.1.8:3000/api/upload`, {
-//       headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-//       method: 'POST',
-//       //body: createFormData(photo.assets[0], {userId: '123'}),
-//       body: body[0],
-//     })
-//       .then(response => response.json())
-//       .then(response => {
-//         console.log('response', response);
-//       })
-//       .catch(error => {
-//         console.log('error nè nha', error);
-//       });
-//   };
-
-//   return (
-//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//       {photo && (
-//         <>
-//           <Image source={{uri: photo.uri}} style={{width: 300, height: 300}} />
-//           <Button title="Upload Photo" onPress={handleUploadPhoto} />
-//         </>
-//       )}
-//       <Button title="Choose Photo" onPress={handleChoosePhoto} />
-//     </View>
-//   );
-// };
-
-// export default App;
-
 // App.js
 
 import React from 'react';
 import {View, Image, Button, Platform} from 'react-native';
 import {launchImageLibrary} from 'react-native-image-picker';
-import axios from 'axios';
-
-const SERVER_URL = 'http://localhost:3000';
+const SERVER_URL = 'http://192.168.1.8:3000';
 
 const createFormData = (photo, body = {}) => {
   const data = new FormData();
 
   data.append('photo', {
-    name: photo.assets[0].fileName,
-    type: photo.assets[0].type,
-    uri:
-      Platform.OS === 'ios'
-        ? photo.assets[0].uri.replace('file://', '')
-        : photo.assets[0].uri,
+    name: photo.fileName,
+    type: photo.type,
+    uri: Platform.OS === 'ios' ? photo.uri.replace('file://', '') : photo.uri,
   });
 
-  data.append('userId', body.userId);
-  console.log(data);
+  Object.keys(body).forEach(key => {
+    data.append(key, body[key]);
+  });
+
   return data;
 };
 
@@ -112,19 +35,23 @@ const App = () => {
 
   const handleUploadPhoto = () => {
     let body = new FormData();
-    // body.append('file', {
-    //   uri: photo.assets[0].uri,
-    //   name: photo.assets[0].name,
-    //   type: photo.assets[0].type,
-    // });
-    body.append('photo', photo.assets[0]);
-    //body.append('Content-Type', 'image/jpeg');
-    //body.append('userId', '456');
-    fetch(`http://192.168.1.8:3000/api/upload`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data',
+    let sourcePhoto = {
+      name: photo.assets[0].fileName,
+      type: photo.assets[0].type,
+      uri: photo.assets[0].uri,
+    };
+    console.log('Source photo', sourcePhoto);
+    body.append(
+      'photo',
+      {
+        name: photo.assets[0].fileName,
+        type: photo.assets[0].type,
+        uri: photo.assets[0].uri,
       },
+      photo.assets[0].uri,
+    );
+    fetch(`${SERVER_URL}/api/upload`, {
+      method: 'POST',
       body: body,
     })
       .then(response => response.json())
@@ -134,6 +61,20 @@ const App = () => {
       .catch(error => {
         console.log('error', error);
       });
+    // var xhr = new XMLHttpRequest();
+    // xhr.open('POST', `${SERVER_URL}/api/upload`);
+    // xhr.send(body);
+    // fetch(`${SERVER_URL}/api/upload`, {
+    //   method: 'POST',
+    //   body: createFormData(photo, {userId: '123'}),
+    // })
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     console.log('response', response);
+    //   })
+    //   .catch(error => {
+    //     console.log('error', error);
+    //   });
   };
 
   return (
--- nodeJs-http-image-server-rn/index.js
+++ nodeJs-http-image-server-rn/index.js
@@ -1,41 +1,37 @@
 // server.js
 
-const express = require("express");
-const multer = require("multer");
-const bodyParser = require("body-parser");
-var fs = require("fs");
-var upload = multer({ dest: "uploads/" });
+const express = require('express');
+const multer = require('multer');
+const bodyParser = require('body-parser');
 
 const app = express();
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 const storage = multer.diskStorage({
   destination(req, file, callback) {
-    callback(null, "./images");
+    callback(null, 'uploads/');
   },
   filename(req, file, callback) {
     callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
   },
 });
 
-//const upload = multer({ storage });
+const upload = multer({storage});
 
-app.get("/", (req, res) => {
-  res.status(200).send("You can post to /api/upload.");
+app.get('/', (req, res) => {
+  res.status(200).send('You can post to /api/upload.');
 });
 
-app.post("/api/upload", upload.single("photo"), (req, res) => {
-  console.log("file", req.photo);
-  //console.log("body", req.body);
-  // console.log("body", req.body.photo.originalname);
-
+app.post('/api/upload', upload.single('photo'), (req, res) => {
+  console.log('file', req.files);
+  console.log('body', req.body);
   res.status(200).json({
-    message: "success!",
+    message: 'success!',
   });
 });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(
-    `server is running at http://localhost:${process.env.PORT || 3000}`
+    `server is running at http://localhost:${process.env.PORT || 3000}`,
   );
 });
